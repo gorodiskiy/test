72846fa073d310f1b145077881c1065227b4b11e
evgeniy <gorodiskiy4@gmail.com>
2019-10-28 13:43:30 +0200

    initial commit

---
+++ js/render.js
@@ -0,0 +1,74 @@
+let casesArr = [
+  {
+    collection: "iPhoneX",
+    name: "Фиолетовый неон кеды",
+    img: "img/1.jpg",
+    atr: "FG098-X-purple",
+    imgposter: "img/1.jpg",
+    price: 50
+  },
+  {
+    collection: "iPhoneX",
+    name: "Фиолетовый неон именной",
+    img: "img/1.jpg",
+    atr: "X-purple-name",
+    imgposter: "img/1.jpg",
+    price: 150
+  },
+  {
+    collection: "iPhoneX",
+    name: "Зеленый неон",
+    img: "img/1.jpg",
+    atr: "X-green-clear",
+    imgposter: "img/1.jpg",
+    price: 100
+  },
+  {
+    collection: "iPhoneX",
+    name: "Зеленый неон глаза",
+    img: "img/1.jpg",
+    atr: "FG098-X-green",
+    imgposter: "img/1.jpg",
+    price: 250
+  },
+  {
+    collection: "iPhoneX",
+    name: "зеленый неон лабиринт",
+    img: "img/case/iPhoneX/X-green labirint.mp4",
+    atr: "TXT427-X-green",
+    imgposter: "img/1.jpg",
+    price: 300
+  },
+  {
+    collection: "iPhoneX",
+    name: "Оранжевый неон girl power",
+    img: "img/1.jpg",
+    atr: "TXT427-X-orange",
+    imgposter: "img/1.jpg",
+    price: 350
+  },
+  {
+    collection: "iPhoneX",
+    name: "Оранжевый неон лабиринт",
+    img: "img/1.jpg",
+    atr: "TEX173-X-orange",
+    imgposter: "img/1.jpg",
+    price: 250
+  },
+  {
+    collection: "iPhoneX",
+    name: "Оранжевый неон leo",
+    img: "img/1.jpg",
+    atr: "TEX171-X-orange",
+    imgposter: "img/1.jpg",
+    price: 200
+  },
+  {
+    collection: "iPhoneX",
+    name: "Оранжевый неон name",
+    img: "img/1.jpg",
+    atr: "X-orange-name",
+    imgposter: "img/1.jpg",
+    price: 400
+  }
+];
